a3cd9c4c7e1055d7218f8556906ab1d24059921e
Shuudoushi <shuudoushi@pc-logix.com>
2015-01-26 18:33:11 -0600

    I think I fixed a few issue...

--- bin/.root.lua
+++ bin/.root.lua
@@ -4,28 +4,28 @@ local event = require("event")
 local shell = require("shell")
 local term = require("term")
 local io = require("io")
-local timer_id
+local timer_id = event.timer(...)
 
 local function root()
  hn = io.open("/tmp/.hostname.dat", "r")
   texthn = hn:read()
    hn:close()
 
-local dir = shell.getWorkingDirectory()
-local userHome = "/usr/home/" .. texthn .. "/"
+ local dir = shell.getWorkingDirectory()
+ local userHome = "/usr/home/" .. texthn .. "/"
 
- if dir ~= userHome and dir <= userHome then
-  print("This action requires root access.")
-   os.sleep(1.5)
-    term.clear()
-    term.setCursor(1,1)
+if dir ~= userHome and dir <= userHome then
+ print("This action requires root access.")
+  os.sleep(1.5)
+   term.clear()
+   term.setCursor(1,1)
   superauth()
  end
- if kill == superauth() then
-  event.cancel(timer_id)
+if kill == superauth() then
+ event.cancel(timer_id)
  end
 end
 
---event.timer(1, root, math.huge)
+event.timer(1, root, math.huge)
 
 timer_id = event.timer(...)
--- lib/superauth.lua
+++ lib/superauth.lua
@@ -13,40 +13,41 @@ event.listen("key_down", check)
 
 return function()
 
-hn = io.open("/tmp/.hostname.dat", "r") -- Reads the hostname file.
- texthn = hn:read()
-  hn:close()
+  hn = io.open("/tmp/.hostname.dat", "r") -- Reads the hostname file.
+   texthn = hn:read()
+    hn:close()
 
-us = io.open("/usernames.dat", "r") -- Reads the root user file.
- textus = us:read()
-  us:close()
+  us = io.open("/usernames.dat", "r") -- Reads the root user file.
+   textus = us:read()
+    us:close()
 
-ps = io.open("/passwords.dat", "r") -- Reads the root password file
- textps = ps:read()
-  ps:close()
+  ps = io.open("/passwords.dat", "r") -- Reads the root password file
+   textps = ps:read()
+    ps:close()
 
-  term.clear()
-  term.setCursor(1,1)
-  term.write("User: ")
-   username = term.read()
-   username = string.gsub(username, "\n", "")
-  term.setCursor(1,2)
-  term.write("Password: ")
-   password = term.read(nil, nil, nil, "")
-   password = string.gsub(password, "\n", "")
- if username == textus and password == textps then
-  term.clear()
-  term.setCursor(1,1)
-  print("Logged in as Root.")
-   os.sleep(1)
-  term.clear()
-  term.setCursor(1,1)
-   os.setenv("PS1", "root" .. "@" .. texthn .. "$ ")
-   shell.setWorkingDirectory("/")
-  return true
- else
-  print("Login failed.")
    shell.setWorkingDirectory("/usr/home/" .. texthn .. "/")
-  return false
+   term.clear()
+   term.setCursor(1,1)
+    term.write("User: ")
+     username = term.read()
+     username = string.gsub(username, "\n", "")
+   term.setCursor(1,2)
+    term.write("Password: ")
+     password = term.read(nil, nil, nil, "")
+     password = string.gsub(password, "\n", "")
+  if username == textus and password == textps then
+   term.clear()
+   term.setCursor(1,1)
+   print("Logged in as Root.")
+    os.sleep(1)
+   term.clear()
+   term.setCursor(1,1)
+    os.setenv("PS1", "root" .. "@" .. texthn .. "$ ")
+    shell.setWorkingDirectory("/")
+   return true
+  else
+   print("Login failed.")
+    shell.setWorkingDirectory("/usr/home/" .. texthn .. "/")
+   return false
  end
 end
