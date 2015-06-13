233a33457023a0fabc71ffe43c9a8059367cb4fb
Shuudoushi <shuudoushi@pc-logix.com>
2015-06-13 00:59:47 -0500

    Added scripts for adding and removing users.

---
+++ bin/deluser.lua
@@ -0,0 +1,13 @@
+local term = require("term")
+local auth = require("auth")
+
+term.clear()
+term.setCursor(1,1)
+term.write("Please enter a username to delete from the system.")
+term.setCursor(1,2)
+term.write("Username: ")
+	username = term.read()
+	username = string.gsub(username, "\n", "")
+	username = string.lower(username)
+
+auth.rmUser(username)
