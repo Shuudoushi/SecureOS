a9e0e223d40782d7c94b5f13b800e0553d7b7106
Shuudoushi <shuudoushi@pc-logix.com>
2015-06-20 01:02:22 -0500

    testing things

--- bin/update.lua
+++ bin/update.lua
@@ -1,10 +1,28 @@
 local computer = require("computer")
 local shell = require("shell")
 local term = require("term")
+local textu = "release"
 
-u = io.open("/etc/update.cfg", "r")
- textu = u:read()
-  u:close()
+local args, options = shell.parse(...)
+	if #args == 0 then
+		u = io.open("/etc/update.cfg", "r")
+		 textu = u:read()
+		  u:close()
+	end
+
+	if args == "dev" then
+		textu = "dev"
+	end
+
+	if args == "release" then
+		textu = "release"
+	end
+
+	if options.a then
+		uw = io.open("/etc/update.cfg", "w")
+		 io.write(tostring(args))
+		  uw:close()
+	end
 
 term.clear()
 term.setCursor(1,1)
@@ -15,23 +33,23 @@ shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .
 	os.sleep(1)
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/root/sudo.lua /root/sudo.lua \n")
 	os.sleep(1)
+shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/root/.root.lua /root/.root.lua \n")
+	os.sleep(1)
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/bin/logout.lua /bin/logout.lua \n")
 	os.sleep(1)
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/bin/usage.lua /bin/usage.lua \n")
 	os.sleep(1)
-shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/root/.root.lua /root/.root.lua \n")
-	os.sleep(1)
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/bin/update.lua /bin/update.lua \n")
-	os.sleep(1)
-shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/lib/sha256.lua /lib/sha256.lua \n")
-	os.sleep(1)
-shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/lib/auth.lua /lib/auth.lua \n")
 	os.sleep(1)
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/bin/adduser.lua /bin/adduser.lua \n")
 	os.sleep(1)
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/bin/deluser.lua /bin/deluser.lua \n")
 	os.sleep(1)
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/bin/uninstall.lua /bin/uninstall.lua \n")
+	os.sleep(1)
+shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/lib/sha256.lua /lib/sha256.lua \n")
+	os.sleep(1)
+shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/lib/auth.lua /lib/auth.lua \n")
 	os.sleep(1)
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/init.lua /init.lua \n")
 	os.sleep(1)
