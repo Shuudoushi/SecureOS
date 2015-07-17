29aa3ce2c066510489bb164ed039e9b5ad8d79e8
Shuudoushi <shuudoushi@pc-logix.com>
2015-07-16 21:09:31 -0500

    Updated 'sudo.lua'. Still needs some polish.

--- root/sudo.lua
+++ root/sudo.lua
@@ -5,18 +5,7 @@ local shell = require("shell")
 local auth = require("auth")
 local term = require("term")
 
--- local running = true
-
 local args = {...}
-
---[[local function check() -- Prevents "ctrl+alt+c" and "ctrl+c".
-  if keyboard.isControlDown() then
-    io.stderr:write("( ͡° ͜ʖ ͡°)")
-    os.sleep(0.1)
-    computer.shutdown(true)
- end
-end
-event.listen("key_down", check)]]
 
 local function suAuth()
   hn = io.open("/tmp/.hostname.dat", "r") -- Reads the hostname file.
@@ -35,18 +24,13 @@ local function suAuth()
       r:write("true")
        r:close()
       username, password = "" -- This is just a "bandaid fix" till I find a better way of doing it.
-      -- event.ignore("key_down", check)
       os.sleep(0.1)
-      shell.setWorkingDirectory("/bin/")
       shell.execute(path)
       shell.execute("rm /tmp/.root")
-    -- running = false
   else
     auth.userLog(username, "root fail")
     io.stderr:write("Login failed.")
     username, password = "" -- This is just a "bandaid fix" till I find a better way of doing it.
-      -- event.ignore("key_down", check)
-    -- running = false
   end
 end
 
@@ -67,29 +51,23 @@ local function suAuthPerm()
       r:write("true")
        r:close()
       username, password = "" -- This is just a "bandaid fix" till I find a better way of doing it.
-      -- event.ignore("key_down", check)
       os.sleep(0.1)
       os.setenv("PS1", "root@" .. texthn .. "# ") -- Sets the user environment.
       shell.setWorkingDirectory("/root")
       os.setenv("HOME", "/root")
       os.setenv("USER", "/root")
-    -- running = false
   else
     auth.userLog(username, "root fail")
     io.stderr:write("Login failed.")
     username, password = "" -- This is just a "bandaid fix" till I find a better way of doing it.
-      -- event.ignore("key_down", check)
-    -- running = false
   end
 end
 
-if #args == 0 then
-  path = shell.resolve(...)
+if #args == 1 then
+  path = shell.getAlias(args[1])
   suAuth()
 elseif #args == 1 and args[1] == "su" then
   suAuthPerm()
+else
+  io.stderr:write("error") -- Too lazy to properly do this bit atm...
 end
-
---[[while running do
-  suAuth()
-end]]
