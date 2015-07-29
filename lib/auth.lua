13d240ba3c8e76e50ed09abb69bb6e3290ad8c50
SuPeRMiNoR2 <superminor12@gmail.com>
2015-07-28 23:10:24 -0500

    Clean, and final test

--- lib/auth.lua
+++ lib/auth.lua
@@ -1,50 +1,49 @@
 --Some of this was made with the help of SuPeRMiNoR2
 --Or maybe all of it
 
+passwdfile = "/etc/passwd"
+
+local auth = {}
 local component = require("component")
 local fs = require("filesystem")
 local sha = require("sha256")
 
 local function toHex(data)
   return (data:gsub('.', function (c)
-    return string.format('%02X', string.byte(c))
+    return string.format('%02x', string.byte(c))
     end))
 end
 
-local function dataCardSum(data)
-	return toHex(datac.sha256(data))
+local function datac256(data)
+  return toHex(datac.sha256(data))
 end
 
 if component.isAvailable("data") then --This should really be cleaned up later
-	datac = component.data
-	sha.sha256 = dataCardSum
+  datac = component.data
+  sha.sha256 = datac256
 end
-
-local auth = {}
-
-passwdfile = "/etc/passwd"
 
 if not fs.exists(passwdfile) then
   f = io.open(passwdfile, "w")
-   f:write()
-    f:close()
+  f:write()
+  f:close()
 end
 
 local function split(str,sep)
-    local array = {}
-    local reg = string.format("([^%s]+)",sep)
-    for mem in string.gmatch(str,reg) do
-        table.insert(array, mem)
-    end
-    return array
+  local array = {}
+  local reg = string.format("([^%s]+)",sep)
+  for mem in string.gmatch(str,reg) do
+    table.insert(array, mem)
+  end
+  return array
 end
 
 local function buildDB()
-    local users = {}
-    u = io.open(passwdfile, "r")
-    raw = u:read("*a")
+  local users = {}
+  u = io.open(passwdfile, "r")
+  raw = u:read("*a")
 
-    if raw ~= nil then
+  if raw ~= nil then
 
     local temp = split(raw, "\n")
 
@@ -53,9 +52,9 @@ local function buildDB()
       users[t[1]] = {password=t[2], su=t[3]}
     end
 
-    end
+  end
 
-    return users
+  return users
 end
 
 local function saveDB(db)
@@ -88,29 +87,29 @@ function auth.rmUser(username)
 end
 
 function auth.validate(username, password)
-    local users = buildDB()
+  local users = buildDB()
 
-    validated = false
-    superuser = false
+  validated = false
+  superuser = false
 
-    local data = users[username]
+  local data = users[username]
 
-    if data ~= nil then
-        if data["password"] == sha.sha256(password) then
-            validated = true
-        end
-        if data["su"] == "1" then
-            superuser = true
-        end
+  if data ~= nil then
+    if data["password"] == sha.sha256(password) then
+      validated = true
     end
-    return validated, superuser
+    if data["su"] == "1" then
+      superuser = true
+    end
+  end
+  return validated, superuser
 end
 
 function auth.userLog(username, arg)
   if not fs.get("/etc/").isReadOnly() then
   userw = io.open("/etc/userlog", "a")
-   userw:write(username .. "|" .. os.date("%F %X") .. "|" .. arg .. "\n")
-    userw:close()
+  userw:write(username .. "|" .. os.date("%F %X") .. "|" .. arg .. "\n")
+  userw:close()
   end
 end
 
