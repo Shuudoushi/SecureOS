7a68af9fe69651e43369716b1522aade12ae410d
Shuudoushi <shuudoushi@pc-logix.com>
2015-07-29 01:06:21 -0500

    Revert "..."
    This reverts commit ecf68c0f7d2659d4f0bd44792666ae9f5bbd4b2c.

--- bin/deluser.lua
+++ bin/deluser.lua
@@ -2,17 +2,42 @@ local fs = require("filesystem")
 local term = require("term")
 local auth = require("auth")
 
-term.clear()
-term.setCursor(1,1)
-term.write("Please enter a username to delete from the system.")
-term.setCursor(1,2)
-term.write("Username: ")
-	username = term.read()
-	username = string.gsub(username, "\n", "")
-	username = string.lower(username)
+if not require("auth").isRoot then
+  io.stderr:write("not authorized")
+  return
+end
 
-auth.rmUser(username)
+local args = {...}
 
-if fs.exists("/usr/home/" .. username .. "/") then
-	fs.remove("/usr/home/" .. username .. "/")
+if #args ~= 0 then
+  username = args[1]
+  username = string.lower(username)
+  auth.rmUser(username)
+  auth.userLog(username, "removed")
+  if fs.exists("/home/" .. username .. "/") then
+    fs.remove("/home/" .. username .. "/")
+  end
+  print(username.. " removed")
+  username = ""
+elseif #args == 0 then
+  term.clear()
+  term.setCursor(1,1)
+  term.write("Please enter a username to delete from the system.")
+  term.setCursor(1,2)
+  term.write("Username: ")
+    username = term.read()
+    username = string.gsub(username, "\n", "")
+    username = string.lower(username)
+
+  auth.rmUser(username)
+  auth.userLog(username, "removed")
+
+  if fs.exists("/home/" .. username .. "/") then
+      fs.remove("/home/" .. username .. "/")
+  end
+
+  username = ""
+
+else
+  return
 end
--- bin/logout.lua
+++ bin/logout.lua
@@ -1,11 +1,12 @@
+local fs = require("filesystem")
 local shell = require("shell")
 local event = require("event")
 
-k = io.open("/tmp/.key", "r")
- textk = k:read()
-  k:close()
+if fs.isAutorunEnabled() == true then
+  fs.setAutorunEnabled(false)
+end
 
-  event.cancel(tonumber(textk))
-
+require("auth").userLog("logout")
+fs.remove("/tmp/.root")
 shell.setWorkingDirectory("/")
-shell.execute("/boot/99_login.lua")
+shell.execute("/boot/z_login.lua")
