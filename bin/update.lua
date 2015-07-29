0ebf01c809340f492c7c9b32f3658f75bf2d8095
Shuudoushi <shuudoushi@pc-logix.com>
2015-07-28 22:46:13 -0500

    tweaked 'update'

--- bin/update.lua
+++ bin/update.lua
@@ -29,44 +29,45 @@ end
 
 local function update(args, options)
 
-    if #args == 0 then
-        u = io.open("/etc/update.cfg", "r")
-         textu = u:read()
-          u:close()
-    end
+  if #args == 0 then
+    u = io.open("/etc/update.cfg", "r")
+      textu = u:read()
+        u:close()
+  end
 
-    if #args == 1 then
-        textu = args[1]
-        textuw = io.open("/tmp/update-tmp.cfg", "w")
-         textuw:write(textu)
-          textuw:close()
-        if textu ~= "dev" and textu ~= "release" then
-            io.stderr:write("Not a vaild repo tree.")
-            return
-        end
+  if #args == 1 then
+    textu = args[1]
+    textuw = io.open("/tmp/update-tmp.cfg", "w")
+      textuw:write(textu)
+        textuw:close()
+    if textu ~= "dev" and textu ~= "release" then
+      io.stderr:write("Not a vaild repo tree.")
+      return
     end
+  end
 
-    if options.a then
-        uw = io.open("/etc/update.cfg", "w")
-         uw:write(tostring(args[1]))
-          uw:close()
-    end
+  if options.a then
+    uw = io.open("/etc/update.cfg", "w")
+      uw:write(tostring(args[1]))
+        uw:close()
+  end
 
+shell.execute("wget -fq https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/tmp/update-tmp.lua /tmp/update-tmp.lua")
 term.clear()
 term.setCursor(1,1)
 print("SecureOS will now update.")
-    os.sleep(1)
-    term.setCursor(1,2)
-shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/tmp/update-tmp.lua /tmp/update-tmp.lua \n")
+  os.sleep(1)
+  term.setCursor(1,2)
 shell.execute("/tmp/update-tmp.lua")
 os.remove("/tmp/update-tmp.lua")
 os.remove("/tmp/update-tmp.cfg")
+require("auth").userLog(os.getenv("USER"), update)
 term.clear()
 term.setCursor(1,1)
 print("Update complete. System restarting now.")
-    os.sleep(2.5)
-    os.remove("/tmp/.root")
-    computer.shutdown(true)
+  os.sleep(2.5)
+  os.remove("/tmp/.root")
+  computer.shutdown(true)
 end
 
 update(args, options)
