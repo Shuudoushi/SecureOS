19115cc5791a6da95d114ad99a389c325ddcb79e
Kodos Atoz <Kodos-Atoz@users.noreply.github.com>
2015-08-03 00:11:34 -0500

    Update debug.lua
    Added an amount parameter for debugging. If no parameter is specified, defaults to 100k

--- lib/debug.lua
+++ lib/debug.lua
@@ -3,10 +3,10 @@ local shell = require("shell")
 
 local debug = {}
 
-function debug.benchmark() -- Not a clue what it could really be used for, but here it is.
+function debug.benchmark(amt) -- Not a clue what it could really be used for, but here it is.
   local x = os.clock()
   local s = 0
-  for i=1,100000 do
+  for i=1,(amt or 100000) do
     s = s + i
   end
     return string.format("elapsed time: %.2f", os.clock() - x)
