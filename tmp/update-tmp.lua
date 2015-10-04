2cfc4036fa515ff05e875674e755b3f62d6f9625
Shuudoushi <ichi_1@live.com>
2015-10-04 15:19:55 -0500

    looks like it has to stick around

--- tmp/update-tmp.lua
+++ tmp/update-tmp.lua
@@ -50,6 +50,7 @@ shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/lib/bit32.lua /lib/bit32.lua \n")
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/bin/cd.lua /bin/cd.lua \n")
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/lib/term.lua /lib/term.lua \n")
+shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/.autorun.lua /.autorun.lua \n")
 os.remove("/tmp/depreciated.dat")
 os.sleep(1.5)
 
