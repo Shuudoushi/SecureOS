0acbb7d9419e471ee1791e1c2735f5a3c9481fc7
Shuudoushi <ichi_1@live.com>
2015-10-11 19:45:27 -0500

    let's see if this works...

--- tmp/update-tmp.lua
+++ tmp/update-tmp.lua
@@ -16,5 +16,9 @@ if not require("filesystem").exists("/sbin") then
 end
 
 shell.execute("wget -f https://raw.githubusercontent.com/Shuudoushi/SecureOS/" .. textu .. "/sbin/update.lua /sbin/update.lua \n")
+local v = io.open("/.verion", "w")
+v:write("myversions = { bin = "tostring(0)", boot = "tostring(0)", etc = "tostring(0)", lib = "tostring(0)", root = "tostring (0)", sbin = "tostring(0)", system = "tostring(0)", usr = "tostring (0)" }")
+v:close()
+print("The updater has changed, please run it again after reboot")
 os.remove("/tmp/depreciated.dat")
 os.sleep(1.5)
