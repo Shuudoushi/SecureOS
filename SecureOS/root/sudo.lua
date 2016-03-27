f1343db6237ede21e3db533b393c173e32a95ae3
Shuudoushi <ichi_1@live.com>
2016-03-26 22:55:53 -0500

    Can't even be bothered.

--- SecureOS/root/sudo.lua
+++ SecureOS/root/sudo.lua
@@ -10,7 +10,7 @@ local hn = io.open("/tmp/.hostname.dat", "r") -- Reads the hostname file.
 
 local function request()
   term.write("[sudo] password for ".. texthn ..": ")
-    local password = string.gsub(term.read(nil, nil, nil, ""), "\n", "")
+    local password = string.gsub(term.read({pwchar=""}), "\n", "")
 end
 
 local history = history or {}
